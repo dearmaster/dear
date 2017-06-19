4e6bd711edc6bd90789c8020d8c4ca07267d253a
dearmaster windows7 <694114533@qq.com>
2017-06-20 02:45:20 +0800

    .

--- dearmaster/src/main/java/com/master/dearmaster/controller/BlogCategoryController.java
+++ dearmaster/src/main/java/com/master/dearmaster/controller/BlogCategoryController.java
@@ -4,26 +4,24 @@ import com.master.dearmaster.model.BlogCategory;
 import com.master.dearmaster.util.LoggerFactory;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.master.dearmaster.Constants.SESSION_LOGIN_USER;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("blogCategory")
-//@SessionAttributes(Constants.SESSION_LOGIN_USER)
 public class BlogCategoryController {
 
     private static final Logger logger = LoggerFactory.getLogger();
 
-    @RequestMapping(method = RequestMethod.GET, value = "load")
+    /*@RequestMapping(method = RequestMethod.GET, value = "load")
     @ResponseBody
     public List<BlogCategory> findAll(Model model, HttpServletRequest request, HttpSession session) {
 
@@ -31,6 +29,22 @@ public class BlogCategoryController {
 
         if(logger.isDebugEnabled()) {
             logger.debug("login user: " + loginId);
+        }
+
+        List<BlogCategory> list = mockupAll();
+        if(logger.isDebugEnabled()) {
+            logger.debug(list);
+        }
+        return list;
+    }*/
+
+    @RequestMapping(method = GET, value = "load/{blogowner}")
+    @ResponseBody
+    public List<BlogCategory> findAll(HttpSession session, @PathVariable String blogowner) {
+
+        if(logger.isDebugEnabled()) {
+            String loginId = (String) session.getAttribute(SESSION_LOGIN_USER);
+            logger.debug(loginId + " is querying the list of blog categories of " + blogowner);
         }
 
         List<BlogCategory> list = mockupAll();
